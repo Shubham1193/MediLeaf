ac39556af514cf88d238fdd79f85ee7d3a288eb3
Shubham1173 <shubhamsingh91930@gmail.com>
2023-10-26 14:02:53 +0530

    adding web url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './components/Home'
+import Details from './components/Details'
+import Identify from './components/Identify';
+import Plants from './components/Plants'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+          <Route path="identify" element= {<Identify />} />
+          <Route path="Plants" element= {<Plants />} />
+          <Route path="details/:name" element={<Details/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
